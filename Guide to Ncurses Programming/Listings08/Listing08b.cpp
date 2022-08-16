17895fb68b5a56609a0301c8ea2efef49a4e61fc
malvarco <malvarco@hotmail.com>
2022-08-15 22:34:42 -0400

    Commit Listings 8 and 9

---
+++ Guide to Ncurses Programming/Listings08/Listing08b.cpp
@@ -0,0 +1,66 @@
+/*
+* Display windows when a user hits the Help key or some other key
+* on the keyboard.
+* Listing 8-2: helpmenu1.c
+*/
+
+#include <curses.h>
+
+void showhelp(void);
+
+WINDOW* help;
+
+int main(void)
+{
+    int ch;
+
+    initscr();
+
+/* build help menu */
+    // if ((help = newwin(0, 0, 0, 0)) == NULL)   // Original source
+    if ((help = newwin(10, 30, 4, 26)) == NULL)      // Added later in book to show window size.
+    {
+        addstr("Unable to allocate window memory\n");
+        endwin();
+        return(1);
+    }
+    /* --- Original in source --- 
+    mvwaddstr(help, 6, 32, "Help menu Screen");
+    mvwaddstr(help, 9, 28, "Press the - key to quit");
+    mvwaddstr(help, 12, 28, "Press ENTER to go back");
+    */
+
+    // Replacement of lines above added later in book to show window size
+    mvwaddstr(help, 1, 7, "Help menu Screen");
+    mvwaddstr(help, 5, 3, "Press the - key to quit");
+    mvwaddstr(help, 8, 4, "Press ENTER to go back");
+
+    /* now start the program loop */
+
+    addstr("Typer Program\n");
+    addstr("Press + for help:\n\n");
+    refresh();
+    noecho();
+    do
+    {
+        ch = getch();
+        refresh();
+        if (ch == '+')
+            showhelp();
+        else
+            addch(ch);
+    } while (ch != '~');
+
+    endwin();
+    return 0;
+    }
+
+void showhelp(void)
+{
+    touchwin(help);             /* Not in original source, added to show use of touchwin() */
+    wrefresh(help);
+    getch();                            /* wait for key press */
+    touchwin(stdscr);           /* Not in original, added by author to explain touchwin() */
+
+    refresh();
+}
---
+++ Guide to Ncurses Programming/Listings09/Listings09d.cpp
@@ -0,0 +1,46 @@
+/*
+*  Remove the subwindow, the parent window must be touched and refreshed.
+* Listing 9-4: kid4.c
+*/
+
+#include <curses.h>
+
+int main(void)
+{
+    WINDOW* grandpa, * father, * boy;
+    int maxx, maxy;
+
+    initscr();
+
+    start_color();                      /* remember to check for errors! */
+    init_pair(1, COLOR_WHITE, COLOR_BLUE);
+    init_pair(2, COLOR_RED, COLOR_YELLOW);
+    init_pair(3, COLOR_CYAN, COLOR_GREEN);
+
+    getmaxyx(stdscr, maxy, maxx);
+
+    /* create windows - remeber to check for errors! */
+    grandpa = newwin(maxy - 4, maxx - 10, 2, 5);
+    father = subwin(grandpa, maxy - 8, maxx - 20, 4, 10);
+    boy = subwin(father, maxy - 16, maxx - 40, 8, 20);
+
+    /* color windows and splash some text */
+    wbkgd(grandpa, COLOR_PAIR(1));
+    waddstr(grandpa, "Granpa\n");
+    wbkgd(father, COLOR_PAIR(2));
+    waddstr(father, "Father\n");
+    wbkgd(boy, COLOR_PAIR(3));
+    waddstr(boy, "Boy");
+    wrefresh(grandpa);
+    getch();
+
+    /* remove the subwindow "boy" */
+    delwin(boy);
+    wclear(father);
+    waddstr(father, "Bye, son!\n");
+    wrefresh(father);
+    getch();
+
+    endwin();
+    return 0;
+}
