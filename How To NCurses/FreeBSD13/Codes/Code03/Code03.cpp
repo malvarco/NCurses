652914d0eb5ab140a807261b09dd4ecc03eb89a0
malvarco <malvarco@hotmail.com>
2022-08-25 22:54:32 -0400

    FreeBSD13 codes howto

---
+++ How To NCurses/FreeBSD13/Codes/Code03/Code03.cpp
@@ -0,0 +1,26 @@
+
+/*
+* Example 3
+* Simple printw
+*/
+
+#include <curses.h>			/* ncurses.h includes stdio.h */
+#include <string.h>
+
+int main()
+{
+	char mesg[] = "Just a string";			/* message to be appeared on the screen */
+	int row, col;							/* to store the number of rows and */
+
+	initscr();						/* start curses mode */
+	getmaxyx(stdscr, row, col);		/* get the number of rows and columns */
+	mvprintw(row / 2, (col - strlen(mesg)) / 2, "%s", mesg);
+									/* print the message at the center of the screen */
+	mvprintw(row - 2, 0, "This screen has %d rows and %d columns\n", row, col);
+	printw("Try resizing your window (if possible) and then run this program again");
+	refresh();
+	getch();
+	endwin();
+
+	return 0;
+}
