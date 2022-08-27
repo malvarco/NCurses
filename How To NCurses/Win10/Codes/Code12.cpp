180d595c936dd044c166510f66acb66c514c5a72
malvarco <malvarco@hotmail.com>
2022-08-26 22:49:53 -0400

    Moves and new Listings

---
+++ How To NCurses/Win10/Codes/Code12.cpp
@@ -0,0 +1,28 @@
+/*
+Get back to cooked mode (normal line buffering mode) temporarily.
+First save the tty modes with a call to def_prog_mode() and then call endwin() to end the curses mode.
+*/
+
+#include <curses.h>
+#include <stdlib.h>
+
+int main()
+{
+    initscr();                              /* Start curses mode */
+    printw("Hello World !!!\n");            /* Print Hello World */
+    refresh();                              /* Print it on to the real screen */
+    getch(); // Added by me not in original source
+    def_prog_mode();                        /* Save the tty modes */
+    endwin();                               /* End curses mode temporarily */
+    //system("/bin/sh"); // Original source /* Do whatever you like in cooked mode */
+    system("pwsh"); // Not in original, go to powershell  /* Do whatever you like in cooked mode */
+    reset_prog_mode();                      /* Return to the previous tty mode */
+                                            /* stored by def_prog_mode() */
+    refresh();                              /* Do refresh() to restore the */
+                                            /* Screen contents */
+    printw("Another String\n");             /* Back to curses use the full */
+    refresh();                              /* capabilities of curses */
+    endwin();                               /* End curses mode */
+    return 0;
+}
+   
