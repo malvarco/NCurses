badb61c46ae16fdb6587b758dc15071b1a6dd6b2
malvarco <malvarco@hotmail.com>
2022-08-30 21:26:09 -0400

    fbsd codes

--- How To NCurses/FreeBSD13/Codes/Code12.cpp
+++ How To NCurses/FreeBSD13/Codes/Code12.cpp
@@ -1,3 +1,4 @@
+
 /*
 Get back to cooked mode (normal line buffering mode) temporarily.
 First save the tty modes with a call to def_prog_mode() and then call endwin() to end the curses mode.
@@ -14,8 +15,8 @@ int main()
     getch(); // Added by me not in original source
     def_prog_mode();                        /* Save the tty modes */
     endwin();                               /* End curses mode temporarily */
-    //system("/bin/sh"); // Original source /* Do whatever you like in cooked mode */
-    system("pwsh"); // Not in original, go to powershell  /* Do whatever you like in cooked mode */
+    system("/bin/sh"); // Original source /* Do whatever you like in cooked mode */
+
     reset_prog_mode();                      /* Return to the previous tty mode */
                                             /* stored by def_prog_mode() */
     refresh();                              /* Do refresh() to restore the */
