2e3e0176c7aca40bee943cf3859183108a69f01e
malvarco <malvarco@hotmail.com>
2022-09-08 12:58:52 -0400

    YT binaries

---
+++ YouTube Casual Coder's NCurses/Sources/YTCode05.cpp
@@ -0,0 +1,47 @@
+/*
+* Ncurses Tutorial 5 - Getting Information about the Terminal
+* (Very much modified by me)
+*/
+
+#include <curses.h>
+#include <string>
+
+using namespace std;
+
+void start_ncurses(bool useRaw, bool useNoecho);
+void printMenu(WINDOW* menu, string choices[], int size, int highlight);
+
+int main(int argc, char** argv)
+{
+	/* NCURSES START */
+	initscr();	
+	noecho();
+	cbreak();
+
+	int y, x, yBeg, xBeg, yMax, xMax;
+
+	WINDOW* win = newwin(10, 20, 10, 10);
+
+	getyx(stdscr, y, x);
+	getbegyx(stdscr,yBeg, xBeg);
+	getmaxyx(stdscr,yMax,xMax);
+
+	printw("getyx y = %d\n", y);
+	printw("getyx x = % d\n", x);
+	printw("getbegyx yBeg = % d\n", yBeg);
+	printw("getbegyx xBeg = % d\n", xBeg);
+	printw("getbegyx yMax = % d\n", yMax);
+	printw("getbegyx xMax = % d\n", xMax);
+	
+	mvprintw(yMax / 2, xMax / 2, "hello");
+	getbegyx(win, yBeg, xBeg);
+
+	printw("\ngetbegyx window win yBeg = % d\n", yBeg);
+	printw("getbegyx window win xBeg = % d\n", xBeg);
+
+	// make sure program waits before exiting
+	getch();
+	endwin();
+	/* NCURSES END*/
+	return 0;
+}
---
+++ YouTube Casual Coder's NCurses/Sources/YTCode07.cpp
@@ -0,0 +1,69 @@
+/*
+* Ncurses Tutorial 7 - Basic Menu System
+*/
+
+#include <curses.h>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	/* NCURSES START */
+	initscr();
+	noecho();
+	cbreak();
+
+	// get screen size
+	int yMax, xMax;
+	getmaxyx(stdscr, yMax, xMax);
+
+	// Create a window for out input
+	WINDOW* menuwin = newwin(6, xMax - 12, yMax - 8, 5);
+	box(menuwin, 0, 0);
+	refresh();
+	wrefresh(menuwin);
+	keypad(menuwin, true);
+
+	string choices[3] = { "Walk", "Jog", "Run" };
+	int choice;
+	int highlight = 0;
+
+	while (1)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			if (i == highlight)
+				wattron(menuwin, A_REVERSE);
+			mvwprintw(menuwin, i + 1, 1, choices[i].c_str());
+			wattroff(menuwin, A_REVERSE);
+		}
+		choice = wgetch(menuwin);
+
+		switch (choice)
+		{
+		case KEY_UP:
+			highlight--;
+			if (highlight == -1)
+				highlight = 2;
+			break;
+		case KEY_DOWN:
+			highlight++;
+			if (highlight == 3)
+				highlight = 0;
+			break;
+		default:
+			break;
+		}
+		if (choice == 10)
+			break;
+		}
+	
+	printw("Your choice was: %s", choices[highlight].c_str());
+
+	// make sure program waits before exiting
+	getch();
+	endwin();
+	/* NCURSES END*/
+	return 0;
+}
